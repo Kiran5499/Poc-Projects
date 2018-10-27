7f697a238ab7de1944f76f199ed1deabe1a77c93
DSPavan <41519377+DSPavan@users.noreply.github.com>
2018-10-27 04:41:33 -0700

    Faculty Pavan

---
+++ dataTtest.R
@@ -0,0 +1,12 @@
+# Data in two numeric vectors
+# ++++++++++++++++++++++++++
+# Weight of the mice / pig before treatment
+before <-c(200.1, 190.9, 192.7, 213, 241.4, 196.9, 172.2, 185.5, 205.2, 193.7)
+# Weight of the mice /pig after treatment
+after <-c(392.9, 393.2, 345.1, 393, 434, 427.9, 422, 383.9, 392.3, 352.2)
+# Create a data frame
+my_data <- data.frame( 
+                group = rep(c("before", "after"), each = 10),
+                weight = c(before,  after)
+                )
+my_data
